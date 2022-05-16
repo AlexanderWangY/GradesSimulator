cb5d0e1bf2d562f2575f28510a41685e30d3535d
Alexander Wang <98280966+AlexanderWangY@users.noreply.github.com>
2022-05-16 13:44:58 -0500

    Add files via upload

---
+++ Potter Jr High/Potter Jr High/Form1.Designer.vb
@@ -0,0 +1,133 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lstGrades = New System.Windows.Forms.ListBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lstStudents = New System.Windows.Forms.ListBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblNumber = New System.Windows.Forms.Label()
+        Me.btnDisplay = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(43, 34)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(36, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "&Grade"
+        '
+        'lstGrades
+        '
+        Me.lstGrades.FormattingEnabled = True
+        Me.lstGrades.Location = New System.Drawing.Point(46, 50)
+        Me.lstGrades.Name = "lstGrades"
+        Me.lstGrades.Size = New System.Drawing.Size(23, 95)
+        Me.lstGrades.Sorted = True
+        Me.lstGrades.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(105, 34)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(49, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "&Students"
+        '
+        'lstStudents
+        '
+        Me.lstStudents.FormattingEnabled = True
+        Me.lstStudents.Location = New System.Drawing.Point(108, 50)
+        Me.lstStudents.Name = "lstStudents"
+        Me.lstStudents.Size = New System.Drawing.Size(87, 95)
+        Me.lstStudents.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(232, 34)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(102, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Number of students:"
+        '
+        'lblNumber
+        '
+        Me.lblNumber.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblNumber.Location = New System.Drawing.Point(235, 50)
+        Me.lblNumber.Name = "lblNumber"
+        Me.lblNumber.Size = New System.Drawing.Size(54, 22)
+        Me.lblNumber.TabIndex = 5
+        '
+        'btnDisplay
+        '
+        Me.btnDisplay.Location = New System.Drawing.Point(235, 133)
+        Me.btnDisplay.Name = "btnDisplay"
+        Me.btnDisplay.Size = New System.Drawing.Size(64, 26)
+        Me.btnDisplay.TabIndex = 6
+        Me.btnDisplay.Text = "&Display"
+        Me.btnDisplay.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(305, 133)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(64, 26)
+        Me.btnExit.TabIndex = 7
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(394, 209)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnDisplay)
+        Me.Controls.Add(Me.lblNumber)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lstStudents)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lstGrades)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Potter Jr High"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lstGrades As ListBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lstStudents As ListBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblNumber As Label
+    Friend WithEvents btnDisplay As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ Potter Jr High/Potter Jr High/Form1.vb
@@ -0,0 +1,29 @@
+﻿Option Infer Off
+Option Strict On
+Option Explicit On
+
+Public Class Form1
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        lstGrades.Items.Add("G")
+        Dim inFile As IO.StreamReader
+        Dim lines As String() = IO.File.ReadAllLines("Grades.txt")
+
+        If IO.File.Exists("Grades.txt") Then
+            inFile = IO.File.OpenText("Grades.txt")
+
+            For intSub As Integer = 0 To lines.Length
+                If lstGrades.Items.Count <= 0 Then
+                    lstGrades.Items.Add(inFile.ReadLine)
+                ElseIf lstGrades.ToString.Length Then
+                Else
+                    lstGrades.Items.Add(inFile.ReadLine)
+                End If
+            Next
+
+        End If
+    End Sub
+End Class
